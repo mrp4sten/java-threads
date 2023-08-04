fc8540a95d783c3569e02b6ce42c49f29ab5b8ca
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-08-04 00:45:56 -0600

    Using sleep/join

--- src/main/java/com/mrp4sten/java/threads/examples/ExampleThreadImplementsJava8.java
+++ src/main/java/com/mrp4sten/java/threads/examples/ExampleThreadImplementsJava8.java
@@ -1,7 +1,9 @@
 package com.mrp4sten.java.threads.examples;
 
 public class ExampleThreadImplementsJava8 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
+
+    Thread main = Thread.currentThread();
 
     Runnable travel = () -> {
       for (int i = 0; i < 10; i++) {
@@ -14,11 +16,25 @@ public class ExampleThreadImplementsJava8 {
         }
       }
       System.out.println("Finally I'm going to travet to: " + Thread.currentThread().getName());
+      System.out.println("Main State: " + main.getState());
     };
 
-    new Thread(travel, "Mauricio Island").start();
-    new Thread(travel, "New Zeland").start();
-    new Thread(travel, "Mexico").start();
-    new Thread(travel, "Canada").start();
+    Thread t1 = new Thread(travel, "Mauricio Island");
+    Thread t2 = new Thread(travel, "New Zeland");
+    Thread t3 = new Thread(travel, "Mexico");
+    Thread t4 = new Thread(travel, "Canada");
+
+    t1.start();
+    t2.start();
+    t3.start();
+    t4.start();
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+
+    // Thread.sleep(10000);
+    System.out.println("Continuing with main method execution: " + main.getName());
   }
 }
