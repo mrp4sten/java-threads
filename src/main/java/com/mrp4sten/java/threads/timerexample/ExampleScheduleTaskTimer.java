585ed054a0821d40b11a5c525e309b9ac74330b3
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-08-11 21:54:05 -0600

    Tasks that repeat

--- src/main/java/com/mrp4sten/java/threads/timerexample/ExampleScheduleTaskTimer.java
+++ src/main/java/com/mrp4sten/java/threads/timerexample/ExampleScheduleTaskTimer.java
@@ -3,22 +3,30 @@ package com.mrp4sten.java.threads.timerexample;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExampleScheduleTaskTimer {
   public static void main(String[] args) {
+
+    AtomicInteger counterAtomic = new AtomicInteger(3);
     Timer timer = new Timer();
     timer.schedule(new TimerTask() {
+
       @Override
       public void run() {
-        System.out.println(
-            "Task do it at: " + new Date() +
-                "Thread name: " + Thread.currentThread().getName());
+        int i = counterAtomic.getAndDecrement();
+        if (i > 0) {
+          System.out.println(
+              "Task" + i + " do it at: " + new Date() +
+                  "Thread name: " + Thread.currentThread().getName());
+        } else {
+          System.out.println("Time ends");
+          timer.cancel();
+        }
 
-        System.out.println("Time ends");
-        timer.cancel();
       }
 
-    }, 5000);
+    }, 5000, 10000);
 
     System.out.println("We schedule a task for 5 more minutes");
   }
